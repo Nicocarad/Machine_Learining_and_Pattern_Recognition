1e4e7af3da833a6b1cbadbdb60ee1905ecd4c656
Nicolò Caradonna <nicocarad@gmail.com>
2023-03-31 14:19:57 +0200

    up

--- Laboratory/Lab4/MVG.py
+++ Laboratory/Lab4/MVG.py
@@ -1,36 +1,41 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
+def vrow(array):
+    return array.reshape((1,array.size))
 
+def vcol(array):
+    return array.reshape((array.size, 1))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def logpdf_GAU_ND(X,mu,C): 
+    
+    C_inv = np.linalg.inv(C) # compute the inverse of the Covariance matrix
+    _,det_log_C = np.linalg.slogdet(C) # compute the determinant og log|C| ( the det is the second parameter of the function)
+    M = C.shape[1]
+    for i in range (X.shape[1]):
+        X_c = X[i] - mu
+        MVG = -M*0.5*np.log(2*np.pi)-0.5*np.log(det_log_C)
+        temp = np.dot(X_c.T,C_inv)
+        temp1 = -0.5*X_c.dot(temp)
+        MVG = MVG + temp1
+        Y = np.hstack(MVG)
+    
+    return Y
+    
+    
 
 if __name__ == '__main__':
     
-    placeholder = 1
+    
+    plt.figure()
+    XPlot = np.linspace(-8, 12, 1000)
+    m = np.ones((1,1)) * 1.0
+    C = np.ones((1,1)) * 2.0
+    plt.plot(XPlot.ravel(), np.exp(logpdf_GAU_ND(vrow(XPlot), m, C)))
+    plt.show()
+ 
+ 
+    pdfSol = np.load('Solution/llGAU.npy')
+    pdfGau = logpdf_GAU_ND(vrow(XPlot), m, C)
+    print(np.abs(pdfSol - pdfGau).max())
