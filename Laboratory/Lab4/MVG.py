a85a11492384a0e0f410aae785b8c5f21e7258e8
Nicolò Caradonna <nicocarad@gmail.com>
2023-04-01 13:22:11 +0200

    fix

--- Laboratory/Lab4/MVG.py
+++ Laboratory/Lab4/MVG.py
@@ -8,15 +8,16 @@ def vrow(array):
 def vcol(array):
     return array.reshape((array.size, 1))
 
-# X is a column vector
-# mu is a colum vector 4x1
-# C is the Covariance matrix 4x4
+# x is a column vector
+# mu is a colum vector M x 1 
+# C is the Covariance matrix M x M 
 def logpdf_GAU_ND_1Sample(x,mu,C): 
     
     C_inv = np.linalg.inv(C) # compute the inverse of the Covariance matrix
     _,det_log_C = np.linalg.slogdet(C) # compute the determinant of log|C| ( the det is the second parameter of the function)
     M = x.shape[0]
     x_c = x - mu
+    
     MVG = - M * 0.5 * np.log(2*np.pi) - 0.5 * det_log_C - 0.5 * np.dot(x_c.T, np.dot(C_inv,x_c))
    
     return MVG
@@ -32,6 +33,7 @@ def logpdf_GAU_ND(X, mu, C):
 
 if __name__ == '__main__':
     
+    # ONE DIMENSIONAL CASE CHECK
     
     plt.figure()
     XPlot = np.linspace(-8, 12, 1000)
@@ -40,7 +42,16 @@ if __name__ == '__main__':
     plt.plot(XPlot.ravel(), np.exp(logpdf_GAU_ND(vrow(XPlot), m, C)))
     plt.show()
  
- 
-    pdfSol = np.load('Solution/llGAU.npy')
+    pdfSol = np.load('utils/llGAU.npy')
     pdfGau = logpdf_GAU_ND(vrow(XPlot), m, C)
     print(np.abs(pdfSol - pdfGau).max())
+    
+    
+    # MULTI DIMENSIONAL CASE CHECK
+    
+    #XND = np.load('Solution/XND.npy')
+    #mu = np.load('Solution/muND.npy')
+    #C = np.load('Solution/CND.npy')
+    #pdfSol = np.load('utils/llND.npy')
+    #pdfGau = logpdf_GAU_ND(XND, mu, C)
+    #print(np.abs(pdfSol - pdfGau).max())
