ace2f138bb407760cfc25adf4df89d808f795eac
Nicolò Caradonna <nicocarad@gmail.com>
2023-04-14 14:22:46 +0200

    f

--- Laboratory/Lab5/Gaussian_Classifier.py
+++ Laboratory/Lab5/Gaussian_Classifier.py
@@ -1,6 +1,10 @@
 import numpy as np
 import sklearn.datasets
 
+def vcol(array):
+    return array.reshape((array.size, 1))
+
+
 # load iris dataset
 def load_iris():
     
@@ -13,7 +17,6 @@ def split_db_2to1(D, L, seed=0):
     np.random.seed(seed) # procduce always the same sequence of random values
     idx = np.random.permutation(D.shape[1]) #compute a permutetion of index: [0,1,2,...,149] to [114,32,11,...,35] randomly
     idxTrain = idx[0:nTrain] # assign the first 2/3 of the indexes to the training set
-    print(idxTrain)
     idxTest = idx[nTrain:] # assign the last 1/3 of the indexes to the test set
     DTR = D[:, idxTrain]
     DTE = D[:, idxTest]
@@ -22,9 +25,29 @@ def split_db_2to1(D, L, seed=0):
     return (DTR, LTR), (DTE, LTE)
 
 
+def mean_and_covariance(D,L):
+    
+    for i in range(L.max()+1):
+        D_c = D[:,L == i]
+        N_c = D_c.shape[1]
+        mu_c = vcol(D_c.mean(1))
+        DC = D_c - mu_c 
+        C_c = np.dot(DC, DC.T)/N_c
+        print("Mean class:", i)
+        print(mu_c) 
+        print("Covariance matrix class:", i)
+        print(C_c)
+        
+    
+    return 
+    
+    
+
 
 if __name__ == '__main__':
     
     D,L = load_iris()
     (DTR, LTR),(DTE,LTE) = split_db_2to1(D,L)
+    mean_and_covariance(D,L)
+    
     
