c626d585b77b62499beae255e8594edac6e001a4
Nicolò Caradonna <nicocarad@gmail.com>
2023-04-14 14:06:57 +0200

    up

---
+++ Laboratory/Lab5/Gaussian_Classifier.py
@@ -0,0 +1,30 @@
+import numpy as np
+import sklearn.datasets
+
+# load iris dataset
+def load_iris():
+    
+    D, L = sklearn.datasets.load_iris()['data'].T, sklearn.datasets.load_iris()['target']
+    return D, L
+
+# divide the dataset in a Training set and Validation Set
+def split_db_2to1(D, L, seed=0):
+    nTrain = int(D.shape[1]*2.0/3.0) # nTrain = 100
+    np.random.seed(seed) # procduce always the same sequence of random values
+    idx = np.random.permutation(D.shape[1]) #compute a permutetion of index: [0,1,2,...,149] to [114,32,11,...,35] randomly
+    idxTrain = idx[0:nTrain] # assign the first 2/3 of the indexes to the training set
+    print(idxTrain)
+    idxTest = idx[nTrain:] # assign the last 1/3 of the indexes to the test set
+    DTR = D[:, idxTrain]
+    DTE = D[:, idxTest]
+    LTR = L[idxTrain]
+    LTE = L[idxTest]
+    return (DTR, LTR), (DTE, LTE)
+
+
+
+if __name__ == '__main__':
+    
+    D,L = load_iris()
+    (DTR, LTR),(DTE,LTE) = split_db_2to1(D,L)
+    
