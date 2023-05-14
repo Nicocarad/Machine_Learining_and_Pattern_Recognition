180fe1933a46976036ed122509d228e4f7bd85b6
Nicolò Caradonna <nicocarad@gmail.com>
2023-05-14 17:11:18 +0200

    g

--- Laboratory/prova.py
+++ Laboratory/prova.py
@@ -3,7 +3,7 @@ import struct
 # apre il file di testo in lettura
 with open('input.txt', 'r') as f:
     # legge i numeri come stringhe e li converte in interi
-    numbers = [int(x) for x in f.read().split(" ")]
+    numbers = [int(x) for x in f.read().split(",")]
 
 # apre il file binario in scrittura
 with open('output.bin', 'wb') as f:
