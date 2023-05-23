fbcd4536352c79ed300460730dc20830e59ce1fa
Nicolò Caradonna <nicocarad@gmail.com>
2023-05-23 19:14:43 +0200

    ROC final

--- Laboratory/Lab8/ROC_curve.py
+++ Laboratory/Lab8/ROC_curve.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 
 
@@ -11,12 +11,6 @@ def confusionMatrix(Real,Predicted):
     for i,j in zip(Predicted.astype(int),Real.astype(int)):
         confMatrix[i, j] += 1
 
-    # Print confusion matrix with labels
-    print("Confusion Matrix:")
-    print("{:>15}{:^10}{:^10}".format("", "Class 0", "Class 1"))
-    print("{:<15}{:^10}{:^10}".format("Prediction 0", confMatrix[0,0], confMatrix[0,1]))
-    print("{:<15}{:^10}{:^10}".format("Prediction 1", confMatrix[1,0], confMatrix[1,1]))
-
     return confMatrix
 
 def optimalBinaryBayesDecision(llr,pi,Cf_n,Cf_p):
@@ -24,3 +18,57 @@ def optimalBinaryBayesDecision(llr,pi,Cf_n,Cf_p):
     t = -np.log(pi*Cf_n/((1-pi)*Cf_p)) # threshold
     Predicted = (llr > t).astype(int)
     return Predicted
+
+
+
+def plot_ROC(scores,LTE):
+    
+    # Concatenate scores with -inf and +inf
+    thresholds = np.concatenate([scores, [-np.inf, np.inf]])
+    # Sort the thresholds
+    thresholds.sort()
+
+    fpr_list = []
+    tpr_list = []
+
+    for t in thresholds:
+    # Compute confusion matrix for current threshold
+        predicted = (scores > t).astype(int)
+        conf_matrix = confusionMatrix(LTE, predicted)
+
+    # TN = confMatrix[0, 0]
+    # FN = confMatrix[0, 1]
+    # FP = confMatrix[1, 0]
+    # TP = confMatrix[1, 1]
+    
+    # Extract false negative rate (FNR) and false positive rate (FPR)
+        (TN, FN), (FP, TP) = conf_matrix
+    
+        fnr = FN / (FN + TP)
+        fpr = FP / (FP + TN)
+
+    # Compute true positive rate (TPR)
+        tpr = 1 - fnr
+
+    # Append FPR and TPR to lists
+        fpr_list.append(fpr)
+        tpr_list.append(tpr)
+
+# Plot ROC curve
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0,1.0])
+    plt.plot(fpr_list, tpr_list)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.savefig("ROC.pdf")
+    plt.show()
+    
+
+if __name__ == '__main__':
+    
+    
+    llr = np.load("Data/commedia_llr_infpar.npy")
+    LTE = np.load("Data/commedia_labels_infpar.npy")
+    
+    plot_ROC(llr,LTE)
